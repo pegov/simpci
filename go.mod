df9b9000ad904e8755f3da9de32a72557142a301
pegov <dvpegov@gmail.com>
2025-07-29 23:01:27 +0300

    add shutdown for tcp accept

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pegov/simpci
 
 go 1.24.4
+
+require golang.org/x/sync v0.16.0
--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"log"
+	"net"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	if len(os.Args) < 3 {
 		log.Fatalf("3 args")
 	}
@@ -35,5 +44,46 @@ func main() {
 	commitPath := path.Join(dataDir, ".commit")
 	if err := os.WriteFile(commitPath, stdoutBuffer.Bytes(), 0777); err != nil {
 		log.Fatalf("failed to write to .commit file: %v", err)
+	}
+
+	g, gCtx := errgroup.WithContext(ctx)
+	l, err := net.Listen("tcp", "127.0.0.1:9095")
+	tcpL := l.(*net.TCPListener)
+	if err != nil {
+		log.Fatalf("net listen: %v", err)
+	}
+	defer l.Close()
+	g.Go(func() error {
+		return server(gCtx, tcpL)
+	})
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	go func() {
+		<-signals
+		cancel()
+		l.Close()
+	}()
+
+	if err := g.Wait(); err != nil {
+		log.Println(err)
+	}
+}
+
+func server(ctx context.Context, l *net.TCPListener) error {
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				log.Println("listener shutdown")
+				return nil
+			default:
+				log.Println("listener error:", err)
+				return errors.New("tcp error")
+			}
+		}
+		defer conn.Close()
 	}
 }
