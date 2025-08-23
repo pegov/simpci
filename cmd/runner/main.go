cbbd1e215f65159aae3d2a94fc0ba9cb94240339
pegov <dvpegov@gmail.com>
2025-08-23 20:32:33 +0300

    add test docker template to run

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"context"
 	"log"
-	"net"
 	"os"
+	"os/exec"
 	"os/signal"
-	"time"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -18,35 +17,41 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 
-	var d net.Dialer
-	conn, err := d.DialContext(ctx, "tcp", "127.0.0.1:9095")
-	if err != nil {
-		log.Fatalf("failed to connect to server: %v", err)
-	}
-
 	go func() {
 		<-signals
 		cancel()
-		conn.Close()
 	}()
 
 	g, ctx := errgroup.WithContext(ctx)
+
 	g.Go(func() error {
-		for {
-			time.Sleep(time.Second * 2)
-			log.Println("writing ping")
-			if _, err := conn.Write([]byte("ping")); err != nil {
-				select {
-				case <-ctx.Done():
-					return nil
-				default:
-					return err
-				}
-			}
-		}
+		return dockerRun(ctx)
 	})
 
 	if err := g.Wait(); err != nil {
 		log.Println("g wait err:", err)
 	}
+}
+
+func dockerRun(ctx context.Context) error {
+	cmd := exec.CommandContext(
+		ctx,
+		"docker",
+		"run",
+		"--rm",
+		"--pull=never",
+		"-v", "./docker/:/tmp/",
+		"simpci-template",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("failed to run docker cmd: %v", err)
+	}
+	if !cmd.ProcessState.Success() {
+		log.Fatalf("docker command's exit code != 0")
+	}
+
+	return nil
 }
---
+++ docker/template.Dockerfile
@@ -0,0 +1,3 @@
+FROM docker:28.3-cli
+
+CMD ["/tmp/template.sh"]
